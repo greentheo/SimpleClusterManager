2e6572e1b4ac0dd90995c85292526ac10014e111
Theodore Van Rooy <munkey906@gmail.com>
2013-10-09 15:45:29 -0600

    more commits

---
+++ R/procStats.R
@@ -0,0 +1,11 @@
+
+#' @param time the amount of time in milliseconds to take the measurement
+#' @author Theodore Van Rooy (\url{http://royaltyanalytics.com/})
+#' @export 
+#' @import lattice
+#' @import httpRequest
+
+getProcStats = function(time=1000){
+  procs = scan('/proc/stats', what=character())
+  
+}
